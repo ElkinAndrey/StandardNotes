3293ce94d19a3cc67455af02f2cd8f895688f7fb
ElkinAndrey <andreyridbir@gmail.com>
2025-02-10 01:10:56 +0500

    Добавил хранилища для сущностей

---
+++ StandardNotesApi/StandardNotesApi/Stores/NoteStore.cs
@@ -0,0 +1,10 @@
+﻿using StandardNotesApi.Entities;
+
+namespace StandardNotesApi.Store
+{
+    public static class NoteStore
+    {
+
+        public static List<Note> Notes { get; set; } = new List<Note>();
+    }
+}
---
+++ StandardNotesApi/StandardNotesApi/Stores/TypeStore.cs
@@ -0,0 +1,9 @@
+﻿using StandardNotesApi.Entities;
+
+namespace StandardNotesApi.Store
+{
+    public static class TypeStore
+    {
+        public static List<NoteType> Types { get; set; } = new List<NoteType>();
+    }
+}
